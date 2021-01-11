c41a26dc1f9c785373cb4cea71723d0720ef2fdc
JeffKi11er <45094606+JeffKi11er@users.noreply.github.com>
2021-01-11 16:08:32 +0700

    upload task 17

--- formHelpNavigation-master/app/src/main/java/com/example/formhelp/practical12/FragmentToolbar.java
+++ formHelpNavigation-master/app/src/main/java/com/example/formhelp/practical12/FragmentToolbar.java
@@ -10,17 +10,29 @@ import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.formhelp.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.jsoup.helper.HttpConnection;
 
 import java.io.InputStream;
@@ -28,7 +40,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class FragmentToolbar extends Fragment implements View.OnClickListener {
+public class FragmentToolbar extends Fragment implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
     private ImageView imgActivitySwipe;
     private ImageView imgReload;
     @Nullable
@@ -37,12 +49,11 @@ public class FragmentToolbar extends Fragment implements View.OnClickListener {
         View view = inflater.inflate(R.layout.fragment_toolbar,container,false);
         return view;
     }
-
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        imgReload = getActivity().findViewById(R.id.img_reload);
-        imgActivitySwipe = getActivity().findViewById(R.id.img_setting);
+        imgReload = getActivity().findViewById(R.id.img_make_reload);
+        imgActivitySwipe = getActivity().findViewById(R.id.img_other);
         imgReload.setOnClickListener(this);
         imgActivitySwipe.setOnClickListener(this);
     }
@@ -50,17 +61,23 @@ public class FragmentToolbar extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.img_reload:
+            case R.id.img_make_reload:
 //                reload();
 //                requestNetwork();
                 //task.execute();
                 break;
-            case R.id.img_setting:
-                startActivity(new Intent(this.getActivity(),PrefActivity.class));
+            case R.id.img_other:
+//                startActivity(new Intent(this.getActivity(),PrefActivity.class));
+                showPopUp(v);
                 break;
         }
     }
-
+    public void showPopUp(View view){
+        PopupMenu popupMenu = new PopupMenu(getContext(),view);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.weather_other);
+        popupMenu.show();
+    }
     private Handler handler1;
     final AsyncTask<String, Integer, Bitmap> task = new AsyncTask<String, Integer, Bitmap>() {
             @Override
@@ -131,5 +148,43 @@ public class FragmentToolbar extends Fragment implements View.OnClickListener {
 
         });
         thread.start();
+    }
+    public void loadWeather(){
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,"https://api.openweathermap.org/data/2.5/weather?q=hanoi&appid=8c941e8d36892c5722dae7bcd92054b2",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.i(getClass().getName(),"Response :"+response);
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            String name = object.getString("name");
+                            String description = object.getString("weather");
+                            //String description = weatherDescription.getJSONArray(0).getString(0);
+//                            JSONObject temp = new JSONObject("wind");
+//                            String degree = temp.getString("deg");
+                            Toast.makeText(getContext(),"location : "+name+", "
+                                    +"status : "+description,Toast.LENGTH_LONG).show();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(),"This not work in that way!",Toast.LENGTH_LONG).show();
+                    }
+                });
+        queue.add(stringRequest);
+    }
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if (item.getItemId()==R.id.menu_ask){
+            loadWeather();
+            return true;
+        }else{
+            return false;
+        }
     }
 }
